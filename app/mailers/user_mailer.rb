9a1618976eeb01fc078cb5cb52297eb8db590132
Courtney Miller <courtneymiller919@gmail.com>
2017-06-12 17:34:03 -0600

    confirmation emails working

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,8 @@
-class SignupMailer < ApplicationMailer
+class UserMailer < ApplicationMailer
   default from: "courtneymiller919@gmail.com"
 
-  def new_signup(user)
+  def new_user(user)
+
      @user = user
 
      mail(to: user.email, subject: "Welcome to Blocipedia, #{user}!")
