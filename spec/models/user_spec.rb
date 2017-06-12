4eaa22634ba15c4c7dc1fdb1edf391c25bae7bcd
Courtney Miller <courtneymiller919@gmail.com>
2017-06-12 17:34:03 -0600

    confirmation emails working

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,5 +2,22 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
+  describe "after_create" do
+ # #22
+     before do
+       @another_comment = Comment.new(body: 'Comment Body', post: post, user: user)
+     end
 
+ # #23
+     it "sends an email to users who have just signed up for an account" do
+       expect(UserMailer).to receive(:new_user).with(user).and_return(double(deliver_now: true))
+
+     end
+
+ # #24
+     it "does not send emails to non-members" do
+       expect(UserMailer).not_to receive(:new_comment)
+ 
+     end
+   end
 end
