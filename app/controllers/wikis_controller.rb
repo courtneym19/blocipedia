c0ee1ec750c2304845794ed17f75704955665345
Courtney Miller <courtneymiller919@gmail.com>
2017-06-20 21:03:44 -0600

    private wiki functionality added

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -17,6 +17,7 @@ class WikisController < ApplicationController
      @wiki = Wiki.new
      @wiki.title = params[:wiki][:title]
      @wiki.body = params[:wiki][:body]
+     @wiki.private = params[:wiki][:private]
 
      if @wiki.save
        flash[:notice] = "Wiki was saved."
@@ -35,6 +36,7 @@ class WikisController < ApplicationController
      @wiki = Wiki.find(params[:id])
      @wiki.title = params[:wiki][:title]
      @wiki.body = params[:wiki][:body]
+     @wiki.private = params[:wiki][:private]
 
      if @wiki.save
        flash[:notice] = "Wiki was updated."
